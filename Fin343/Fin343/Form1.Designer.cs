eee260357e61d2f0f6b9fd992fcc8f306f41cd09
jennygerstle@gmail.com <jennygerstle@gmail.com>
2021-12-29 12:58:50 -0500

    Added code

---
+++ Fin343/Fin343/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Fin343
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tbTicker = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.chrtPrices = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dgvDump = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chrtPrices)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDump)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbTicker
+            // 
+            this.tbTicker.Location = new System.Drawing.Point(12, 34);
+            this.tbTicker.Name = "tbTicker";
+            this.tbTicker.Size = new System.Drawing.Size(49, 20);
+            this.tbTicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ticker";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFrom.Location = new System.Drawing.Point(67, 34);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(89, 20);
+            this.dtpFrom.TabIndex = 2;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTo.Location = new System.Drawing.Point(162, 34);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(89, 20);
+            this.dtpTo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Start Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "EndDate";
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(266, 18);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 37);
+            this.btnGetData.TabIndex = 6;
+            this.btnGetData.Text = "Get Data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // chrtPrices
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chrtPrices.ChartAreas.Add(chartArea2);
+            this.chrtPrices.Location = new System.Drawing.Point(12, 61);
+            this.chrtPrices.Name = "chrtPrices";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Series1";
+            this.chrtPrices.Series.Add(series2);
+            this.chrtPrices.Size = new System.Drawing.Size(329, 223);
+            this.chrtPrices.TabIndex = 7;
+            this.chrtPrices.Text = "chart1";
+            // 
+            // dgvDump
+            // 
+            this.dgvDump.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDump.Location = new System.Drawing.Point(392, 86);
+            this.dgvDump.Name = "dgvDump";
+            this.dgvDump.Size = new System.Drawing.Size(242, 184);
+            this.dgvDump.TabIndex = 8;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(640, 71);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 409);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dgvDump);
+            this.Controls.Add(this.chrtPrices);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTicker);
+            this.Name = "Form1";
+            this.Text = "Closing Prices";
+            ((System.ComponentModel.ISupportInitialize)(this.chrtPrices)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDump)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrtPrices;
+        private System.Windows.Forms.DataGridView dgvDump;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Fin343/Fin343/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Fin343
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = null;
+
+            try
+            {
+                //Change this to your local machine
+                sqlCon = new SqlConnection("Server=LAPTOP-S1SI6Q9Q\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
+                sqlCon.Open(); // open a connection to the data base specified by sqlCon
+                String symbol = tbTicker.Text;
+
+                SqlCommand sqlCmd = new SqlCommand("spGetPrcForSymbol", sqlCon);  
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.Add("@Symbol", System.Data.SqlDbType.VarChar).Value = symbol;
+                sqlCmd.Parameters.Add("@MinPrc", System.Data.SqlDbType.Float).Value = 0.0;
+                //this does not work:
+                sqlCmd.Parameters.Add("@MinDat", System.Data.SqlDbType.VarChar).Value = dtpFrom.Value.ToString();
+                //sqlCmd.Parameters.Add("@MinDat", System.Data.SqlDbType.SmallDateTime).Value = dtpFrom.Value; //.ToString();
+                sqlCmd.ExecuteNonQuery();
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+                DataSet dataset = new DataSet();
+                da.Fill(dataset, "prices");
+                chrtPrices.DataSource = dataset.Tables[0];
+                var table = dataset.Tables[0];
+                int nrPoints = table.Rows.Count;
+                for (int ii = 0; ii < nrPoints; ++ii)
+                {
+                    chrtPrices.Series[0].Points.AddXY(table.Rows[ii][0], table.Rows[ii][1]);
+                }
+                Form1.ActiveForm.Text = symbol + " Closing Prices";
+                dgvDump.DataSource = table;
+            }
+
+            catch (Exception ex)
+
+            {
+                MessageBox.Show(" " + DateTime.Now.ToLongTimeString() + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally
+
+            {
+
+                if (sqlCon != null && sqlCon.State == System.Data.ConnectionState.Open)
+
+                    sqlCon.Close();
+
+            }
+
+        }
+    }
+}
