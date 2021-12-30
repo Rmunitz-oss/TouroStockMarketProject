4f7288a0da097cdb22cfed204d941e6c520d4a29
Rmunitz-oss <58051290+Rmunitz-oss@users.noreply.github.com>
2021-12-30 16:34:01 -0500

    added comments explaining sql connection code

--- Fin343/Fin343/Form1.cs
+++ Fin343/Fin343/Form1.cs
@@ -15,27 +15,51 @@ namespace Fin343
 
         private void btnGetData_Click(object sender, EventArgs e)
         {
-            SqlConnection sqlCon = null;
+            SqlConnection sqlConnection = null;
 
             try
             {
                 //Change this to your local machine
-                sqlCon = new SqlConnection("Server=LAPTOP-S1SI6Q9Q\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
-                sqlCon.Open(); // open a connection to the data base specified by sqlCon
-                String symbol = tbTicker.Text;
+                //Jenn
+                //sqlCon = new SqlConnection("Server=LAPTOP-S1SI6Q9Q\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
+                //Rochel
+                sqlConnection = new SqlConnection("Server=LAPTOP-GOS64JAP\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
+                sqlConnnection.Open(); // open a connection to the data base specified by sqlConnection
+                
+               
+                SqlCommand sqlCommand = new SqlCommand("write SQL queries directly here", sqlConnection);  
+                //sqlCommand.CommandType = CommandType.StoredProcedure; //?
 
-                SqlCommand sqlCmd = new SqlCommand("spGetPrcForSymbol", sqlCon);  
-                sqlCmd.CommandType = CommandType.StoredProcedure;
-                sqlCmd.Parameters.Add("@Symbol", System.Data.SqlDbType.VarChar).Value = symbol;
-                sqlCmd.Parameters.Add("@MinPrc", System.Data.SqlDbType.Float).Value = 0.0;
+                //add parameter to query
+                //example
+                //get ticker user input in GUI
+                String ticker = tbTicker.Text;
+                SqlCommand command = new SqLCommand ("SELECT * FROM MARKETDATA WHERE Ticker = @Ticker", sqlConnection);
+                command.Parameters.Add(new SqlParamater("Ticker", ticker));
+
+                //using a sql data reader to read the data results from the command
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read()){
+                    //read data into variables etc
+                }
+                
+                /*
+                sqlCommand.Parameters.Add("@Symbol", System.Data.SqlDbType.VarChar).Value = symbol;
+                sqlCommand.Parameters.Add("@MinPrc", System.Data.SqlDbType.Float).Value = 0.0;
                 //this does not work:
                 sqlCmd.Parameters.Add("@MinDat", System.Data.SqlDbType.VarChar).Value = dtpFrom.Value.ToString();
                 //sqlCmd.Parameters.Add("@MinDat", System.Data.SqlDbType.SmallDateTime).Value = dtpFrom.Value; //.ToString();
                 sqlCmd.ExecuteNonQuery();
+                */
 
-                SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+                //rather than reading line by line: fill a DataSet
+                //example:
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCommand);
                 DataSet dataset = new DataSet();
-                da.Fill(dataset, "prices");
+                String srcTable = "MarketData";
+                dataAdapter.Fill(dataset, srcTable);
+
+                //fill chart on gui based on dataset 
                 chrtPrices.DataSource = dataset.Tables[0];
                 var table = dataset.Tables[0];
                 int nrPoints = table.Rows.Count;
@@ -57,9 +81,9 @@ namespace Fin343
 
             {
 
-                if (sqlCon != null && sqlCon.State == System.Data.ConnectionState.Open)
+                if (sqlConnection != null && sqlConnection.State == System.Data.ConnectionState.Open)
 
-                    sqlCon.Close();
+                    sqlConnection.Close();
 
             }
 
