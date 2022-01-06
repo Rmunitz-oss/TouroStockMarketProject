2c56c4507a50930fb1b141c910ee7257aa78ab2b
jgerstle2 <58050214+jgerstle2@users.noreply.github.com>
2022-01-06 00:04:14 -0500

    Fixed math to reflect updated stored procedure

--- Fin343/Fin343/Form1.cs
+++ Fin343/Fin343/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Data;
 using System.Windows.Forms;
@@ -23,11 +23,11 @@ namespace Fin343
             {
                 //Change this to your local machine
                 //Jenn
-                //sqlConnection = new SqlConnection("Server=LAPTOP-S1SI6Q9Q\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
+                sqlConnection = new SqlConnection("Server=LAPTOP-S1SI6Q9Q\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
                 //rochel
-                sqlConnection =
+                /*sqlConnection =
                     new SqlConnection(
-                        "Server=LAPTOP-GOS64JAP\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");
+                        "Server=LAPTOP-GOS64JAP\\SQLEXPRESS;Database=StockMarketProject;Trusted_Connection=True;");*/
                 sqlConnection.Open();
 
                 //get user's input
@@ -58,64 +58,94 @@ namespace Fin343
                 DataColumn transactionPrices = dataSet.Tables[0].Columns[2];
                 DataColumn closingPrices = dataSet.Tables[0].Columns[3];
                 DataColumn tradingPnLs = dataSet.Tables[0].Columns[4];
+                DataColumn previousColsingPrice = dataSet.Tables[0].Columns[5];
               
-
-                DataRowCollection marketDataRows = dataSet.Tables[0].Rows;
-               
                 
+                DataRowCollection marketDataRows = dataSet.Tables[0].Rows;
+                foreach(var item in marketDataRows[0].ItemArray)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine();
+
                 //calculations
                 int nrRows = marketDataRows.Count;
+                Console.WriteLine(nrRows);
+                Console.WriteLine("-------------");
                 
                 //b. total quantity = trade quantity + prior total quantity
-                double totalQuantity = 0;
-                double [] totalQuantities = new double [nrRows];
+                decimal totalQuantity = 0;
+                decimal [] totalQuantities = new decimal [nrRows];
                 //first row, totalQuantity = trade quantity
-                DataRow firstRow = marketDataRows[0];
-                totalQuantity += (double) firstRow[1];
-                totalQuantities[0] = totalQuantity;
-                int index = 1;
+
+                //DataRow firstRow = marketDataRows[0];
+                //totalQuantity += (int) firstRow[1]; //change decimal to int
+                //totalQuantities[0] = totalQuantity;
+
+                int index = 0;
                 foreach (DataRow row in marketDataRows)
                 {
-                    totalQuantity += (double)row[1];
+                    totalQuantity += (int)row[1]; //change decimal to int
                     totalQuantities[index] = totalQuantity;
                     index++; 
                 }
-                
+                foreach(var item in totalQuantities)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine("-------------");
+
                 //c. position pnl = quantity (current closing price - prior closing price)
-                Double[] positionPnLs = new Double[nrRows];
+                decimal[] positionPnLs = new decimal[nrRows];
                 //first row (has no previous row) always zero (?)
-                positionPnLs[0] = 0;
-                for (int ix = 1; ix <= nrRows; ix++)
+                //positionPnLs[0] = 0;
+                for (int ix = 0; ix < nrRows; ix++)
                 {
                     DataRow row = marketDataRows[ix];
-                    DataRow previousRow = marketDataRows[ix - 1];
-                    double quantity = (double) row[1];
+                    //DataRow previousRow = marketDataRows[ix - 1];
+                    decimal quantity = (int) row[1];
                     if (quantity < 0)
                     {
                         quantity = totalQuantities[ix]; 
                     }
-                    double closingPrice = (double) row[3];
-                    double previousClosingPrice = (double) previousRow[3];
-                    double positionPnL = quantity * (closingPrice - previousClosingPrice);
+                    decimal closingPrice = (decimal) row[3];
+                    decimal previousClosingPrice = row.IsNull("PreviousClosingPrice") ? (decimal) row[3] : (decimal) row[5];
+                    decimal positionPnL = quantity * (closingPrice - previousClosingPrice);
                     positionPnLs[ix] = positionPnL;
                 }
-                
+                foreach (var item in positionPnLs)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine("-------------");
+
                 //d. total daily pnl = trading pnl + position pnl
-                Double[] totalDailyPnLs = new Double[nrRows];
-                for (int ix = 0; ix <= nrRows; ix++)
+                decimal[] totalDailyPnLs = new decimal[nrRows];
+                for (int ix = 0; ix < nrRows; ix++)
                 {
                     DataRow row = marketDataRows[ix];
-                    double tradingPnl = (double) row[4];
+                    decimal tradingPnl = (decimal) row[4];
                     totalDailyPnLs[ix] = tradingPnl + positionPnLs[ix];
                 }
-                
+                foreach (var item in totalDailyPnLs)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine("-------------");
+
                 //e. cumulative pnl = prior cumulative + total daily pnl
-                Double[] cumulativePnLs = new Double[nrRows];
+                //TODO ADD CHECKER IF ONLY ONE ROW
+                decimal[] cumulativePnLs = new decimal[nrRows];
                 cumulativePnLs[0] = totalDailyPnLs[0];
-                for (int ix = 1; ix <= nrRows; ix++)
+                for (int ix = 1; ix < nrRows; ix++)
                 {
                     cumulativePnLs[ix] = cumulativePnLs[ix - 1] + totalDailyPnLs[ix];
                 }
+                foreach (var item in cumulativePnLs)
+                {
+                    Console.Write(item + ", ");
+                }
+                Console.WriteLine("-------------");
 
 
 
